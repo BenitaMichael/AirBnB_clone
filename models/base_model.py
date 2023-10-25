1b31628df67d92b268fc5a0e91fb78711ad656ba
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-10-24 19:40:51 -0700

    place.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,51 @@
 #!/usr/bin/python3
-"""Base Model (Parent class)"""
-
+"""The Base Model Class"""
 import uuid
-from models import dataStorage
 from datetime import datetime
+import models
 
 
 class BaseModel:
-
-    """Parent Class which all other classes will inherit from"""
+    """The Base Model class"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes like id: uuid,
-        and dates when created and updated
+        """initializes a new base model.
 
         Args:
-            - *args: list of arguments
-            - **kwargs: key-values pair arguments
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
 
-        date_format = '%Y-%m-%dT%H:%M:%S.%f'
-        if kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], date_format)
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], date_format)
-                else:
-                    self.__dict__[key] = kwargs[key]
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    format = "%Y-%m-%dT%H:%M:%S.%f"
+                    value = datetime.strptime(value, format)
+                elif key == '__class__':
+                    continue
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            dataStorage.new(self)
-
-    def __str__(self):
-        """Returns string representation of instance, arguments, date and id"""
-
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+            models.storage.new(self)
 
     def save(self):
-        """
-        Method to update the date of the public instance attribute
-        updated_at
-        """
-
+        """updates the updated at time"""
         self.updated_at = datetime.now()
-        dataStorage.save()
+        models.storage.save()
 
     def to_dict(self):
+        """returns a dictionary containing
+        all keys/values of __dict__ of the instance
         """
-        Returns a dictionary containing all keys/values pairs of __dict_
-        and date in string format
-        """
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = type(self).__name__
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
+        return model_dict
 
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = type(self).__name__
-        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
-        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
-        return obj_dict
+    def __str__(self):
+        """str method"""
+        class_name = type(self).__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
